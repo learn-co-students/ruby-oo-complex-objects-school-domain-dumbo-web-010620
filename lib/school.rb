2344200ca7153a24c7c3f9960486ad457647d532
Jin Wu <jinn.wuu@gmail.com>
2020-01-08 04:08:56 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+class School
+  
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)      # roster => {9 => ["Zach Morris"]}
+    if @roster[grade] == nil                # if it is nil then
+      @roster[grade] = []                   # returns empty array
+        @roster[grade] << student_name      # add student_name to empty array
+    else
+      @roster[grade] << student_name        # if array exist, just add student_name
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.select do |grade, name|
+      @roster[grade] = name.sort
+    end
+    @roster
+  end
+  
+end
